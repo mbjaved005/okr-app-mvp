7dd4715255dedb941066b5d4475f266c9d861e85
Muhammad Bin Javed <muhammad.javed@emumba.com>
2024-12-13 18:37:40 +0400

    Updated overall config files to cater for the build errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
