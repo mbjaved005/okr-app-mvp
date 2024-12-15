619c8f0e852126e192a69ca71a97c0ece3851df4
Muhammad Bin Javed <muhammad.javed@emumba.com>
2024-12-15 15:01:22 +0400

    update routes path

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -16,7 +16,7 @@ const okrRoutes = require("../../routes/okr");
 const log = logger("server");
 // Define routes
 const apiRouter = express.Router();
-app.use("/api", apiRouter);
+app.use("", apiRouter);
 
 if (!process.env.DATABASE_URL || !process.env.SESSION_SECRET) {
   log.error("Error: DATABASE_URL or SESSION_SECRET variables in .env missing.");
@@ -99,21 +99,21 @@ app.use((req, res, next) => {
 
 // Define routes
 app.use(authenticateWithToken);
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userManagementRoutes);
-app.use("/api/okrs", okrRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userManagementRoutes);
+app.use("/okrs", okrRoutes);
 
 // Auth routes
 app.use(
-  "/api/auth",
+  "/auth",
   (req, res, next) => {
-    log.info(`Received request to /api/auth: ${req.method} ${req.url}`);
+    log.info(`Received request to /auth: ${req.method} ${req.url}`);
     next();
   },
   authRoutes
 );
 
-app.get("/api/auth/test", (req, res) => {
+app.get("/auth/test", (req, res) => {
   res.json({ message: "Auth route is working" });
 });
 
@@ -123,12 +123,12 @@ app.use("/uploads", (req, res, next) => {
   next();
 });
 
-app.use("/api/users", userManagementRoutes);
-app.use("/api/okrs", okrRoutes); // Integrate OKR routes
+app.use("/users", userManagementRoutes);
+app.use("/okrs", okrRoutes); // Integrate OKR routes
 /////////////////////////////
 
 // File upload endpoint
-app.post("/api/upload", upload.single("profilePicture"), (req, res) => {
+app.post("/upload", upload.single("profilePicture"), (req, res) => {
   log.info("Received file upload request");
   if (req.file) {
     log.info(`File uploaded successfully: ${req.file.path}`);
